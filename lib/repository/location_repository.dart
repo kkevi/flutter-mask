611006b4552ce3acc911d9697bbf3aca65cadfd1
kkevi <borakim@simbaat.com>
2022-12-21 13:43:20 +0900

    permission 추가

--- lib/repository/location_repository.dart
+++ lib/repository/location_repository.dart
@@ -1,9 +1,8 @@
 import 'package:geolocator/geolocator.dart';
 
 class LocationRepository {
-  // final _geolocator = Geolocator();
-
   Future<Position> getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
 
